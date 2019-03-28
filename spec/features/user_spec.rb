144bda293729fe043885e81b0093661d2774ece9
Noah Flint <noah.flint24@gmail.com>
2019-03-28 15:29:26 -0600

    finished up to logging in a user, still does not use authorize method

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,6 +6,28 @@ RSpec.describe 'as a visitor' do
       visit root_path
 
       click_link "Register"
+
+      fill_in "user_name", with: "noah"
+      fill_in "user_email", with: "email@email.com"
+      fill_in "user_password", with: "1234"
+
+      click_on "Create User"
+
+      expect(current_path).to eq(root_path)
+      expect(User.last.name).to eq("noah")
+    end
+    it 'can register new user' do
+      visit root_path
+
+      click_link "Log In"
+
+      fill_in "email", with: "email@email.com"
+      fill_in "password", with: "1234"
+
+      click_on "Login"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("You have successfully logged in")
     end
   end
 end
